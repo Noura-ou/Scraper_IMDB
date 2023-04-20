94cba905aede93ef328e6aa83f9a598f8ce23c00
Noura Ous <noura.ousfia@gmail.com>
2023-04-20 12:27:50 +0200

    ajout css

--- scrap_imdb/scrap_imdb/streamlit_app.py
+++ scrap_imdb/scrap_imdb/streamlit_app.py
@@ -3,19 +3,26 @@ from utils import collection
 import streamlit as st
 
 
+st.set_page_config(
+        page_icon=":film_frames:",
+        layout="wide",
+        initial_sidebar_state="expanded",
+    )
 
 def main():
-    st.title("Mon application de recherche de films")
-    st.write("## Les plus longs films")
+    st.markdown("<h1 style='color: #256550 ; text-align:center;'> Mon application de recherche de films </h1>", unsafe_allow_html=True)
+  
+    st.markdown(f"<h2 style='color: green;'>Les plus longs films : </h2>", unsafe_allow_html=True)
     longest = utils.longest_movie(collection)
     st.write(longest)
 
 
-    st.write("## Les 5 films les mieux notés")
+    st.markdown(f"<h2 style='color: green;'>Les 5 films les mieux notés : </h2>", unsafe_allow_html=True)
     top_film_note = utils.top_film_note(collection)
     st.write(top_film_note)
 
-    st.write("## Dans combien de films a joué Morgan Freeman ? Tom Cruise ?.....")
+
+    st.markdown(f"<h2 style='color: green;'> Dans combien de films a joué Morgan Freeman ? Tom Cruise ?..... </h2>", unsafe_allow_html=True)
     actor = collection.distinct("acteurs")
     # affichage des genres sous forme de liste déroulante dans Streamlit
     actor = st.selectbox("Choisissez un acteur :", actor)
@@ -23,7 +30,7 @@ def main():
     st.write(count_movies)
 
 
-    st.write("## Meilleurs films par genre")
+    st.markdown(f"<h2 style='color: green;'>Meilleurs films par genre : </h2>", unsafe_allow_html=True)
     # récupération des genres disponibles dans la collection
     genre = collection.distinct("genre")
     genre = st.selectbox("Choisissez le genre du film :", genre)
@@ -31,7 +38,7 @@ def main():
     st.write(utils.top_movies_by_genre(genre, n))
 
 
-    st.write("## Parmi les 100 films les mieux notés, quel pourcentage sont américains ? Français ?")
+    st.markdown(f"<h2 style='color: green;'>Parmi les 100 films les mieux notés, quel pourcentage sont américains ? Français ?.... </h2>", unsafe_allow_html=True)
     pays = collection.distinct("pays")
     pays = st.selectbox("Choisissez un pays :", pays)
     n = st.number_input("Nombre de résultats à afficher", min_value=1, max_value=100, value=100, step=1)
@@ -39,14 +46,14 @@ def main():
     st.write(top_film_note)
 
 
-    st.write("## la durée moyenne d’un film en fonction du genre")
+    st.markdown(f"<h2 style='color: green;'>La durée moyenne d’un film en fonction du genre :</h2>", unsafe_allow_html=True)
     genre = collection.distinct("genre")
     genre = st.selectbox("Choisissez un genre pour le film :", genre)
     average_time_by_genre = utils.average_time_by_genre(genre)
     st.write(average_time_by_genre)
 
 
-    st.write("## En fonction du genre, afficher la liste des films les plus longs.")
+    st.markdown(f"<h2 style='color: green;'>En fonction du genre, afficher la liste des films les plus longs : </h2>", unsafe_allow_html=True)
     genre = collection.distinct("genre")
     genre = st.selectbox("Choisissez un genre de film :", genre)
     n = st.number_input("Nombre de résultats à afficher", min_value=1, max_value=100, value=5, step=1)
@@ -54,27 +61,28 @@ def main():
     st.write(longest_movies_by_genre)
 
 
-    st.write("## En fonction du genre, quel est le coût de tournage d’une minute de film ?")
+    st.markdown(f"<h2 style='color: green;'>En fonction du genre, quel est le coût de tournage d’une minute de film ? </h2>", unsafe_allow_html=True)
     genre = collection.distinct("genre")
     genre = st.selectbox("Choisissez un genre de film dont vous voulez calculer le coût de tournage d’une minute ! ", genre)
     cout_tournage = utils.cost_per_minute_by_genre(genre)
     st.write(cout_tournage)
 
 
-    st.sidebar.title("Menu de recherche")
+    #st.sidebar.title("Menu de recherche")
+    st.sidebar.markdown("<h4 style='font-size: 24px; color: #256550;'> Menu de filtre des films : </h4>", unsafe_allow_html=True)
     search_option = st.sidebar.radio(
     "Choisissez le type de recherche :", 
-    ("Par titre", "Par acteur(s)", "Par genre", "Par durée", "Par note")
+    ("Par titre", "Par genre", "Par durée", "Par note")
       )
     
 
-    st.write("## FIltrer les films en utilisant pluieurs filtres ")
+    st.markdown(f"<h2 style='color: green;'> Filtrer les films en utilisant pluieurs filtres  : </h2>", unsafe_allow_html=True)
     # affichage des résultats en fonction du choix de recherche
     if search_option == "Par titre":
         titre_original = collection.distinct("titre_original")
         titre_original = st.selectbox("Choisissez le titre du film :", titre_original)
         result = utils.search_movie_by_title(titre_original)
-        for movie in result:
+        for movie in result[1]:
             st.markdown(f"<h5 style='color: green;'>La date de sortie du film : </h5>", unsafe_allow_html=True)
             st.write(movie["date"])
             st.markdown(f"<h5 style='color: green;'>Acteurs : </h5>", unsafe_allow_html=True)
@@ -101,14 +109,6 @@ def main():
             st.write(movie["budget"])
 
 
-
-    elif search_option == "Par acteur(s)":
-        selected_actors = collection.distinct("acteurs")
-        # affichage des acteurs sous forme de liste déroulante multiselect dans Streamlit
-        selected_actors = st.multiselect("Choisissez un ou plusieurs acteurs :", selected_actors)
-        result = utils.search_movie_by_actor(selected_actors)
-        for result in result:
-           st.write(result["titre_original"])
 
     elif search_option == "Par genre":
         genre = collection.distinct("genre")
